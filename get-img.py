81833c4984fd5850fb34841a2c661771ef510053
Evan Dorsky <evan.dorsky@icloud.com>
2015-01-13 23:17:15 -0500

    Add David, progress prints to get-img

--- get-img.py
+++ get-img.py
@@ -6,6 +6,7 @@ def updateperson(person):
 	token = "a5e92af24222b2218d6b6a2c073ef017bf67123c"
 	userdata = requests.get("https://api.github.com/users/"+person["github"], auth=(token, "")).json()
 
+	print person["name"]
 	person["image"] = userdata["avatar_url"]
 	person["githuburl"] = "http://www.github.com/"+person["github"]
 
@@ -19,6 +20,8 @@ with open("./jade/in.json") as jsonfile:
 index["teachers"] = map(updateperson, index["teachers"])
 index["ninjas"] = map(updateperson, index["ninjas"])
 index["students"] = map(updateperson, index["students"])
+
+print "Done!"
 
 with open("./jade/index.json", "w") as jsonfile:
 	json.dump(index, jsonfile, indent=4, separators=(',', ': '))
